98cf578aaa48707581c40e1ca28c64d87a7e47e1
camilo <camiloesteban661@gmail.com>
2024-07-14 23:51:10 -0500

    feat: implement view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,23 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cmoisdead.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
   plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+  overrides: [{
+    files: "*.astro",
+    options: {
+      parser: "astro"
+    }
+  }],
   markdown: {
     remarkPlugins: ["remark-math"],
-    rehypePlugins: ["rehype-katex"],
-  },
+    rehypePlugins: ["rehype-katex"]
+  }
 });
